5e83a790fb3f2ea68c882c6d0643c6118098a096
Steve Gergens <steve.cdsweb@gmail.com>
2019-03-12 12:53:32 -0500

    Add high and low temperatures

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
         callback('Unable to find location.')
     } else {
-    callback(undefined, body.daily.data[0].summary + ' It is currently ' + Math.round(body.currently.temperature) + ' degrees out. There is a ' + body.currently.precipProbability * 100 + '% chance of precipitation.')
+    callback(undefined, body.daily.data[0].summary + ' It is currently ' + Math.round(body.currently.temperature) + ' degrees out. Today\'\s forecasted high temperature is ' + Math.round(body.daily.data[0].temperatureHigh) + ' with a low temperature of ' + Math.round(body.daily.data[0].temperatureLow) + '. There is a ' + body.currently.precipProbability * 100 + '% chance of precipitation.')
     }
 })
 }
